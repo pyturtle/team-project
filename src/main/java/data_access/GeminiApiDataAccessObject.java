2debd326ee33ed0dc549cc3dde51f33cabffc051
Nathan Nguyen <Nathan-nguyen@outlook.com>
2025-11-25 15:06:49 -0500

    Revert "Ilian interactor tests"

--- src/main/java/data_access/GeminiApiDataAccessObject.java
+++ src/main/java/data_access/GeminiApiDataAccessObject.java
@@ -1,7 +1,6 @@
 package data_access;
 
 import okhttp3.*;
-import org.json.JSONArray;
 import org.json.JSONObject;
 import use_case.plan.generate_plan.GeneratePlanDataAccessInterface;
 import com.google.genai.Client;
@@ -32,13 +31,16 @@ public class GeminiApiDataAccessObject implements GeneratePlanDataAccessInterfac
                             + "." +
                             "Generate a json of the following format:" +
                             "For the plan, the key-value pairs should be: " +
-                            "1. name - plan name string" +
-                            "2. description - plan description string" +
-                            "3. subgoals - json array of subgoals" +
+                            "1. id - (generate unique guid)" +
+                            "2. name - plan name string" +
+                            "3. description - plan description string" +
+                            "3. user_email - sample email string" +
+                            "4. subgoals - json array of subgoals" +
                             "For each subgoal, the key-value pairs should be: " +
-                            "1. name - subgoal name string" +
-                            "2. description - subgoal description string" +
-                            "3. deadline - subgoal description YYYY-MM-DD string" +
+                            "1. id - (generate unique guid)" +
+                            "2. name - subgoal name string" +
+                            "3. description - subgoal description string" +
+                            "4. deadline - subgoal description YYYY-MM-DD string" +
                             "IMPORTANT: Do not include anything else in the response apart from the json object.",
                     null);
             String rawResult = response.text();
@@ -49,41 +51,12 @@ public class GeminiApiDataAccessObject implements GeneratePlanDataAccessInterfac
                     ? rawResult.substring(first + 1, last)
                     : "";
             JSONObject responseObject = new JSONObject(responseText);
-            boolean isValid = validateJsonObject(responseObject);
-            if (isValid) {
-                return prepareResponse(responseObject, "Plan generated successfully!",
-                        true);
-            }
-            else {
-                return prepareResponse(new JSONObject(), "Something went wrong. Please, try again.",
-                        false);
-            }
+            return prepareResponse(responseObject, "Plan generated successfully!", true);
         }
         catch (Exception e) {
             return prepareResponse(new JSONObject(), "Something went wrong. Please, try again.",
                     false);
         }
-    }
-
-    private boolean validateJsonObject(JSONObject responseObject) {
-        boolean isValid;
-        try {
-            isValid = responseObject.has("name") &&
-                    responseObject.has("description") &&
-                    responseObject.has("subgoals");
-            JSONArray subgoals =  responseObject.getJSONArray("subgoals");
-            int i = 0;
-            while(isValid) {
-                JSONObject subgoal = subgoals.getJSONObject(i);
-                isValid = subgoal.has("name") &&
-                        subgoal.has("description") &&
-                        subgoal.has("deadline");
-                LocalDate.parse(subgoal.getString("deadline"));
-            }
-        } catch (Exception e) {
-            isValid = false;
-        }
-        return isValid;
     }
 
     private JSONObject prepareResponse(JSONObject responseObject, String responseMessage, boolean success) {
